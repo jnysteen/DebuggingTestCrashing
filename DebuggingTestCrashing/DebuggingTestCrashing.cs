16ddaa340d11982f3231c3b761be139c858a942d
Jesper Nysteen <code@jnysteen.com>
2022-03-02 20:39:09 +0100

    add well-formatted, tiny examples in V2

--- DebuggingTestCrashing/DebuggingTestCrashing.cs
+++ DebuggingTestCrashing/DebuggingTestCrashing.cs
@@ -8,22 +8,33 @@ public class DebuggingTestCrashing
 {
     [Theory]
     [InlineData(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)]
-    public async Task DoesNotCrash2(int p1, int p2,
+    public async Task Params13IntsDoesNotCrash(int p1, int p2,
         int p3, int p4,
         int p5, int p6, int p7,
         int p8, int p9, int p10,
         int p11, int p12, int p13)
+    {
+        Console.WriteLine("test does not crash");
+    }
+    
+    [Theory]
+    [InlineData(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)]
+    public async Task Params11IntsCrashes(int p1, int p2,
+        int p3, int p4,
+        int p5, int p6, int p7,
+        int p8, int p9, int p10,
+        int p11)
     {
         Console.WriteLine("test crashes");
     }
     
     [Theory]
     [InlineData(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)]
-    public async Task Crashes2(int p1, int p2,
+    public async Task Params11Ints2DoublesCrashes2(int p1, int p2,
         int p3, int p4,
-        int p5, double p6, double p7,
+        int p5, int p6, int p7,
         int p8, int p9, int p10,
-        int p11, int p12, int p13)
+        int p11, double p12, double p13)
     {
         Console.WriteLine("test crashes");
     }
--- DebuggingTestCrashing/DebuggingTestCrashingV2.cs
+++ DebuggingTestCrashing/DebuggingTestCrashingV2.cs
@@ -7,8 +7,26 @@ namespace DebuggingTestCrashing;
 public class DebuggingTestCrashingV2
 {
     [Theory]
+    [InlineData(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)]
+    public async Task TenParams_DoesNotCrash(
+        int p1,
+        int p2,
+        int p3,
+        int p4,
+        int p5,
+        int p6,
+        int p7,
+        int p8,
+        int p9,
+        int p10
+    )
+    {
+        Console.WriteLine("test crashes");
+    }
+    
+    [Theory]
     [InlineData(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)]
-    public async Task Crashes2(
+    public async Task ElevenParams_Crashes(
         int p1,
         int p2,
         int p3,
@@ -21,6 +39,26 @@ public class DebuggingTestCrashingV2
         int p10,
         int p11
         )
+    {
+        Console.WriteLine("test crashes");
+    }
+    
+    [Theory]
+    [InlineData(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)]
+    public async Task TwelveParams_DoesNotCrash(
+        int p1,
+        int p2,
+        int p3,
+        int p4,
+        int p5,
+        int p6,
+        int p7,
+        int p8,
+        int p9,
+        int p10,
+        int p11,
+        int p12
+    )
     {
         Console.WriteLine("test crashes");
     }
